844427c20a04363ed3cf06c582d284ecc7c8786f
zardarz <zakreashlaibah@gmail.com>
2025-05-25 14:06:52 -0500

    Adding player idle, walking animation, and made the player look towards the mouse when it is held down

--- Survival Game/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Survival Game/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     [Header("Settings")]
     [SerializeField] private float speed;
@@ -11,18 +13,35 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 movement;
 
     void Start() {
-        // get the rigidbody at the start of the game
+        // get the rigidbody, sprite rednerer and animator at the start of the game
         rb = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
     void Update() {
         // get the x and y inputs
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if(movement.x == 1) {spriteRenderer.flipX = false;}
+        if(movement.x == -1) {spriteRenderer.flipX = true;}
+
+        flipPlayerBasedOnMousePos();
+
+        if(movement.x != 0 || movement.y != 0) {animator.SetBool("IsMoving", true);}else {animator.SetBool("IsMoving", false);}
     }
 
     void FixedUpdate() {
         // move the player based on the movemnt vector
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void flipPlayerBasedOnMousePos() {
+        if(!Input.GetMouseButton(0)) {return;} // make sure the player is holding the mouse down
+
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if(mousePos.x - transform.position.x > 0) {spriteRenderer.flipX = false;} else {spriteRenderer.flipX = true;}
     }
 }
